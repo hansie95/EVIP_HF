d936a31b1f40681c0e6b101426193af39b6186d5
hansie95 <hansie.schmidt95@gmail.com>
2020-11-20 13:58:29 +0100

    Add project files.

---
+++ Evip_hf/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Evip_hf
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void toSplash_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(typeof(SplashScreen));
+        }
+    }
+}
